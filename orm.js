ac224988ac5ed811996e28130f761ea72e550224
Andrew R Smith <63675982+AndrewCodesDontYaKnow@users.noreply.github.com>
2020-07-15 19:56:30 -0500

    started with file structure and installing npms

---
+++ orm.js
@@ -0,0 +1,11 @@
+// import mysql connection?
+
+// create class orm with constructor connection, then this.connection=connection
+
+// to the class, add the functions that select specific departments, roles, employees, or tables of the groups, and also a function that will add new employees, depts, roles, 
+
+// also a function to return total salaries of departments
+// starter code for the addition function:
+// var sum = function(numOne, numTwo) {
+//     return numOne + numTwo;
+//   };
